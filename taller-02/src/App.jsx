a3a7e5f3930d286d537bfe725e046047eb3b1b8b
JhonMi <minottaj@uninorte.edu.co>
2026-03-10 15:24:34 -0500

    Validation Counter=0

--- taller-02/src/App.jsx
+++ taller-02/src/App.jsx
@@ -97,7 +97,7 @@ function Nosotros() {
       <h2>Cuántos estudiantes van a inscribirse?</h2>
       <p>Usa los botones para ajustar el número</p>
       <div>
-        <button onClick={() => setCounter(counter - 1)}>-</button>
+        <button onClick={() => setCounter(counter==0 ? 0: counter -1)}>-</button>
         <span>{counter}</span>
         <button onClick={() => setCounter(counter + 1)}>+</button>
       </div>
